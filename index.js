efe3795e74288f53b1b3be97aac29eff00a75e5d
Teju <tejumoluwaogunleye@gmail.com>
2025-08-26 20:02:42 +0100

    Challenge Completed

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const urlDatabase = [];
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +21,32 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const findUrl = (url) => {
+  return urlDatabase.find(item => item.shortUrl === parseInt(url) || item.longUrl === url);
+}
+
+app.post('/api/shorturl', (req, res) => {
+  let longUrl = req.body.url;
+  let shortUrl = urlDatabase.length + 1;
+  const existing = findUrl(longUrl);
+  if (!existing) {
+    urlDatabase.push({ longUrl: longUrl, shortUrl: shortUrl });
+    res.json(urlDatabase[urlDatabase.length - 1]);
+  } else {
+    res.json( existing);
+  }
+})
+
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const reqshortUrl = req.params.shortUrl;
+  const originalUrl = findUrl(reqshortUrl);
+  if (originalUrl){
+    res.redirect(originalUrl.longUrl);
+  }else{
+    res.json({error: "Short URL not found"});
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
